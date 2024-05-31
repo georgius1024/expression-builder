fb7d7e0924a4a8d17cebaf86d7125a7abf33b465
Yurii Timofeev <georgius@ural.ru>
2024-05-31 20:13:10 +0500

    added Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import "@fontsource/roboto"
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
